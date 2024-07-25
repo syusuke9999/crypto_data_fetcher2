0424c367a38617265b5e27d560c3638c2c6f56f1
syusuke9999 <6879863+syusuke9999@users.noreply.github.com>
2024-07-26 02:55:06 +0900

    fix

--- crypto_data_fetcher/gmo.py
+++ crypto_data_fetcher/gmo.py
@@ -6,7 +6,6 @@ import urllib.request
 import urllib.error
 from .utils import create_null_logger
 
-
 def url_exists(url):
     try:
         time.sleep(1)
@@ -19,12 +18,13 @@ def url_exists(url):
         print(f"Error: {str(e)} for URL: {url}")
     return False
 
-
 def url_read_csv(url):
     try:
         time.sleep(1)
         df = pd.read_csv(url)
-        df = df.rename(columns={'symbol': 'market'})
+        df = df.rename(columns={
+            'symbol': 'market',
+        })
         df['price'] = df['price'].astype('float64')
         df['size'] = df['size'].astype('float64')
         df['timestamp'] = pd.to_datetime(df['timestamp'], utc=True)
@@ -35,7 +35,6 @@ def url_read_csv(url):
     except Exception as e:
         print(f"Error: {str(e)} for URL: {url}")
     return None
-
 
 class GmoFetcher:
     def __init__(self, logger=None, ccxt_client=None, memory=None):
@@ -65,9 +64,12 @@ class GmoFetcher:
         date = datetime.date(start_year, start_month, 1)
 
         dfs = []
-        while date < today:
-            url = 'https://api.coin.z.com/data/trades/{}/{}/{:02}/{:02}_{}.csv.gz'.format(
+        date_range = pd.date_range(start=date, end=today - datetime.timedelta(days=1), freq='D')
+        for date in date_range:
+            url = 'https://api.coin.z.com/data/trades/{}/{}/{:02}/{:04}{:02}{:02}_{}.csv.gz'.format(
                 market,
+                date.year,
+                date.month,
                 date.year,
                 date.month,
                 date.day,
@@ -88,8 +90,6 @@ class GmoFetcher:
                     ], axis=1)
                 dfs.append(df)
 
-            date += datetime.timedelta(days=1)
-
         if len(dfs) == 0:
             self.logger.debug("No data found for the specified period and market.")
             return pd.DataFrame()  # 空のデータフレームを返す
@@ -106,16 +106,17 @@ class GmoFetcher:
 
         for year in range(2018, today.year + 1):
             url = 'https://api.coin.z.com/data/trades/{}/{}/'.format(market, year)
-            self.logger.debug(url)
+            self.logger.debug(f"Checking URL: {url}")
             if self._url_exists(url):
                 start_year = year
                 break
 
         for month in range(1, 13):
             url = 'https://api.coin.z.com/data/trades/{}/{}/{:02}/'.format(market, start_year, month)
-            self.logger.debug(url)
+            self.logger.debug(f"Checking URL: {url}")
             if self._url_exists(url):
                 start_month = month
                 break
 
         return start_year, start_month
+
